20ae83f6b523a12b1346bb2dbe20c767d592ddc7
Forrest <inbox1043@gmail.com>
2023-10-22 15:22:36 +0800

    add api key

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,11 @@
 from flask import Flask, request, jsonify, render_template
+from decouple import config
 import openai
-
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
-
 
 app = Flask(__name__)
 
-def get_api_key_from_vault():
-    key_vault_uri = "https://keyopenai.vault.azure.net/"
-    credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=key_vault_uri, credential=credential)
-    return client.get_secret("OPENAI-API-KEY").value
-
 # Initialize OpenAI API
-
-openai.api_key = get_api_key_from_vault()
+openai.api_key = "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"
 
 @app.route('/')
 def index():
