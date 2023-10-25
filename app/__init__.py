022cec7ce35686682fce2f0e70040a69d0b151b3
Forrest <inbox1043@gmail.com>
2023-10-25 22:30:19 +0800

    update functions and prompt

--- app/__init__.py
+++ app/__init__.py
@@ -29,22 +29,26 @@ def correct_text():
     text = request.json.get('text')
     refinementType = request.json.get('refinementType')
 
-    base_message = f"Do not change the language, refine the following text, based on the 'requirement': \"{text}\""
+    base_message = f"You are Mr Lin who are good at anything. Text: \"{text}\""
 
     if refinementType == "grammar":
-        instruction = base_message + "requirement: refine only the grammar while maintaining its original language."
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine only the grammar while maintaining its original language."
     elif refinementType == "original":
-        instruction = base_message + "requirement: refine only the grammar while maintaining its original language and tone."
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine only the grammar while maintaining its original language and tone."
     elif refinementType == "formal":
-        instruction = base_message + "requirement: refine into a formal tone maintaining its original language."
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine into a formal tone maintaining its original language."
     elif refinementType == "casual":
-        instruction = base_message + "requirement: refine into a casual tone maintaining its original language."
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine into a casual tone maintaining its original language."
     elif refinementType == "serious":
-        instruction = base_message + "requirement: refine into a serious tone maintaining its original language."
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine into a serious tone maintaining its original language."
     elif refinementType == "motivational":
-        instruction = base_message + "requirement: refine into a motivational tone maintaining its original language."
-    else: # humorous
-        instruction = base_message + "requirement: refine into a humorous tone maintaining its original language."
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine into a motivational tone maintaining its original language."
+    elif refinementType == "humorous":
+        instruction = base_message + "Do not change the language, refine the text, based on the 'requirement'. requirement: refine into a humorous tone maintaining its original language."
+    elif refinementType == "translation":
+        instruction = base_message + "if the text is Chinese Language, translate the text to fluent and natural British English. if the text is English, translate the text to fluent and natural Chinese Language"
+    else: # Ask Mr Lin Anything
+        instruction = base_message + "Answer the question only the question is approriate, which is not evil question. Answer the question in the same language of the question. meaning if the text is chinese reply in chinese, if the text is in english, reply in enlglish"
 
     messages = [
         {"role": "system", "content": "You are a helpful assistant who is good at both English and Chinese."},
